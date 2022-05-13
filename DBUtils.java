4ba2f33f053ada274a3adefe5cc3d0deee616036
Rudravaramvenkatesh <Your Email>
2022-05-13 21:32:39 +0530

    stash command used

--- DBUtils.java
+++ DBUtils.java
@@ -1,2 +1,3 @@
+I started using stash command and currently in development
 I am using dev branch
 This file contain DB related logic code
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+started using stash command
 This line  updating in master branch
 This file contains the deployment script
 This will contain all deployment related informations
